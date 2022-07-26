3e4ebb5633eb6e54537cc8ad9cf02764e6f32a1a
Sopyan Sauri <sopyansauri09@gmail.com>
2022-07-27 05:45:01 +0700

    seeders

---
+++ database/seeders/CampBenefitTableSeeder.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\CampBenefit;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+class CampBenefitTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $CampBenefits = [
+            [
+                'camp_id' => 1,
+                'name' => 'Pro Techstack kit'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'iMac pro 2021 & Display'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => '1-1 Mentoring Program'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Final Project Certificate'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Offline Course Videos'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Future Job Opportinity'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Premium Design Kit'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Website Builder'
+            ],
+            [
+                'camp_id' => 2,
+                'name' => 'Mentoring program'
+            ],
+            [
+                'camp_id' => 2,
+                'name' => 'Final Project Certificate'
+            ],
+            [
+                'camp_id' => 2,
+                'name' => 'Offline Course Videos'
+            ],
+        ];
+
+        CampBenefit :: insert($CampBenefits);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        
+        $this->call([
+            CampTableSeeder::class,
+            CampBenefitTableSeeder::class,
+            AdminUserSeeder::class,
+        ]);
     }
 }
